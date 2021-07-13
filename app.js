5bb4f20c4f80b203b0dbc0bc7cbdf88d3a95c3d5
Emilio <emiliobatan@ymail.com>
2021-07-12 20:01:32 -0700

    added board and CS

--- app.js
+++ app.js
@@ -1,4 +1,36 @@
-// State 
+// testing
+let testElem = document.querySelector('#board');
+console.log('testElem: ', testElem);
 
-let state = {
+// State 
+let state = {}; 
+
+function resetState () { 
+    state.board = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+};
+
+//  DOM SELECTORS 
+let boardElem = document.querySelector('#board'); 
+
+// DOM MANIPULATION FUNCTIONS 
+function renderBoard () { 
+    // How to empty an Element
+    boardElem.innerText = '';
+    for(let i=0; i<state.board.length; ++i) { 
+        let grid = state.board[i];
+        // console.log('grid: ', grid);
+        let boxElem = document.createElement('div');
+        boxElem.classList.add('box');
+        // console.log('boxElem: ', boxElem);
+        boxElem.innerText = grid;
+        boardElem.appendChild(boxElem);
+    }
 }
+
+// EVENT LISTENERS 
+
+
+
+// BOOSTRAPPING 
+resetState();
+renderBoard();
