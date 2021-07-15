b2414ee9be45f5a303625a8c72fae57f6b99a517
Emilio <emiliobatan@ymail.com>
2021-07-14 20:29:06 -0700

    Added the player name inputs, as well as event listener

--- app.js
+++ app.js
@@ -5,12 +5,32 @@
 // State 
 const state = {}; 
 
-let resetState = () => { 
-    state.board = ['', '', '', '', '', '', '', '', '']; //  Function will wipe out the state and start over 
-};
+const resetState = () => { 
+    // state.board = ['', '', '', '', '', '', '', '', '']; //  Function will wipe out the state and start over 
+    state.board = [ 
+        {value: '', isTurned: false},
+        {value: '', isTurned: false},
+        {value: '', isTurned: false},
+        {value: '', isTurned: false},
+        {value: '', isTurned: false},
+        {value: '', isTurned: false},
+        {value: '', isTurned: false},
+        {value: '', isTurned: false},
+        {value: '', isTurned: false},
+    ];
+    state.getCurrentPlayer = () => state.players[state.currentPlayerIdx];
+    state.players = ['', ''];
+    state.currentPlayerIdx = 0;
+}
 
 //  DOM SELECTORS 
-let boardElem = document.querySelector('#board'); 
+const boardElem = document.querySelector('#board'); 
+let playerTurnElem = document.querySelector('#playerTurn');
+
+//  HELPER FUNCTIONS 
+const changeTurn = () => { 
+    state.currentPlayerIdx = (state.currentPlayerIdx + 1) % 2;
+}
 
 // DOM MANIPULATION FUNCTIONS 
 const renderBoard = () => { 
@@ -22,23 +42,60 @@ const renderBoard = () => {
         boxElem.classList.add('box');
         // console.log('boxElem: ', boxElem);
         boxElem.dataset.index = i;     // Grabs data from the board 
-        boxElem.innerText = grid;      // This renders to the grid
+        if(grid.isTurned) boxElem.innerText = grid.value;      // This renders to the grid
         boardElem.appendChild(boxElem);    // This creates the boxElem to go to the DOM
     }
 }
 
+const renderPlayer = () => { 
+    let text;
+
+    if (!state.players[0] || !state.players[1]) {
+      text = `
+        <input name='player1' placeholder='Player 1'>
+        <input name='player2' placeholder='Player 2'>
+        <button class="start">Start</button>
+      `
+    } else {
+      // if we do have players
+      text = `It's currently ${state.getCurrentPlayer()}'s turn`
+    }
+
+    playerTurnElem.innerHTML = text;
+  }
+
+const render = () => { 
+    renderBoard();
+    renderPlayer();
+}
+
 // EVENT LISTENERS 
 boardElem.addEventListener('click', function(event) { 
-    if (event.target.className === 'box') { 
+    if (event.target.className !== 'box') return;
         console.log('event.target: ', event.target);
-        let boxIdx = event.target.dataset.index;
+        const boxIdx = event.target.dataset.index;
         console.log('boxIdx: ', boxIdx);
-        state.board[boxIdx] = 'X'
-        renderBoard();
-    }
+        state.board[boxIdx].isTurned = true;
+        changeTurn();
+        console.log('state.currentPlayerIdx: ', state.currentPlayerIdx);
+        console.log('state.getCurrentPlayer(): ', state.getCurrentPlayer());
+        render();
+    
+})
+
+playerTurnElem.addEventListener('click', function(event) { 
+    if (event.target.className !== 'start') return;
+    const player1Name = document.querySelector('input[name=player1]');
+    state.players[0] = player1Name.value;
+    console.log('state.players: ', state.players[0]);
+    const player2Name = document.querySelector('input[name=player2]');
+    state.players[1] = player2Name.value;
+    console.log('state.players: ', state.players[1]);
+    // console.log('foo');
+    render();
 })
 
 
 // BOOSTRAPPING 
 resetState();
-renderBoard();
+render();
