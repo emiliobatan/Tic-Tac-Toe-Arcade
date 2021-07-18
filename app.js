a95b9a7bdbe6b11883e59819a98662c5d38c2f3f
Emilio <emiliobatan@ymail.com>
2021-07-18 14:40:29 -0700

    Cleaned codde and made check winner function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // State 
 const state = {};
-
+state.currentPlayerIdx = 0;
 const resetState = () => { 
   state.board = [
     ['','',''],
@@ -13,6 +13,7 @@ const resetState = () => {
     state.currentPlayerIdx = 0;
 }
 
+// let flatList = []; 
 // const winner = [ 
 //     [0,1,2],
 //     [3,4,5],
@@ -24,43 +25,112 @@ const resetState = () => {
 //     [2,4,6]
 // ];
 
+// const winner = { 
+//   0:
+//   1:
+//   2:
+//   3: 
+//   4:
+//   5: 
+//   6:
+//   7:
+//   8:
+// }
+
 // true = X | false = O
-let isPlayer1 = true;
+// let isPlayer1 = true;
+
 
 //  DOM SELECTORS 
 const boardElem = document.querySelector('#board'); 
 const playerTurnElem = document.querySelector('#playerTurn');
 
+
 //  HELPER FUNCTIONS 
 const changeTurn = () => { 
-  state.currentPlayerIdx = (state.currentPlayerIdx + 1) % 2;
-
-  if(isPlayer1 == true) {
-    isPlayer1 = false;
-  } else {
-    isPlayer1 = true;
+  // state.currentPlayerIdx = (state.currentPlayerIdx + 1) % 2;
+  if (state.currentPlayerIdx === 0) { 
+    state.currentPlayerIdx = 1;
+    return
   }
+  else { 
+    state.currentPlayerIdx = 0;
+    return
+  }
+
+  // if(isPlayer1 == true) {
+  //   isPlayer1 = false;
+  // } else {
+  //   isPlayer1 = true;
+  // }
   // isPlayer1 = !isPlayer1;
 }
 
-function flattenList(list) { 
-  let flatList = []; 
-
-  list.forEach(element => {
-    if (Array.isArray(element)) { 
-      flatList = flatList.concat(flattenList(element));
-    }
-    else { 
-      flatList.push(element);
-    }
-  });
-  
-  return flatList;
+const flattenBoard = () => { 
+  let flatBoard = [].concat.apply([], state.board);
+  return flatBoard;
 }
 
+
+const checkWinner = () => { 
+  const newBoard = flattenBoard(); 
+  console.log('check winner: ', newBoard);
+  if (newBoard[0] === newBoard[1] && newBoard[1] === newBoard[2]) { 
+    console.log('Winner is ', `${state.getCurrentPlayer()}`);
+  }
+
+} 
+
+// }
 // function equals(a, b, c){ 
 //   return a === b && b === c && a!= '';
 // }
+
+// function checkMoves() { 
+//   let winner = null; 
+//   if (equals(state.board[rowIdx][0], state.board[rowIdx][1], state.board[rowIdx][2])) { 
+//     winner = state.board[rowIdx][0];
+//   }
+
+//   // Checks if horizontal rows have won 
+// }
+// console.log(flattenBoard(state.board));
+// let flatBoard = [].concat.apply([], state.board);
+
+// function flattenList (list) { 
+//   let board = state.board
+//   let flatList = []; 
+//   for (let i = 0; i < board.length: i++) { 
+//     for (let j= 0; j < )
+//   }
+// }
+
+// function flattenList(list) { 
+//   let flatList = []; 
+
+//   list.forEach(element => {
+//     if (Array.isArray(element)) { 
+//       flatList = flatList.concat(flattenList(element));
+//     }
+//     else { 
+//       flatList.push(element);
+//     }
+//   });
+  
+//     return flatList;
+
+// const newBoard = flattenList(state.board);
+
+// const winningMove = (player) => { 
+//   if(flattenList(state.board)[0] === player) { 
+//     if (flattenList(state.board)[1] === player && flattenList(state.board)[2]) { 
+//       console.log('winner');
+//       return true;
+//     }
+//   }
+// }
+
+
 // function checkMoves() { 
 //   let winner = null; 
 //   // Checking for Horizontal winners 
@@ -118,22 +188,29 @@ boardElem.addEventListener('click', function(event) {
   // console.log('columnIdx: ', columnIdx);
   const rowIdx = event.target.dataset.row;
   // console.log(isPlayer1);
-  if(isPlayer1 == true) {
+  if(state.currentPlayerIdx === 0) {
     state.board[rowIdx][columnIdx] = 'X';
   } else {
     state.board[rowIdx][columnIdx] = 'O';
   }
-  console.log(rowIdx);
-  console.log(columnIdx);
-  console.log(state)
-  console.log(state.board)
-  console.log('Flat List', flattenList(state.board));
+  
+  // console.log(rowIdx);
+  // console.log(columnIdx);
+  // console.log(state)
+  // console.log(state.board)
+  // console.log('Flat List', flattenList(state.board));
   // console.log(state.board);
   changeTurn();
   // console.log('state.currentPlayerIdx: ', state.currentPlayerIdx);
   // console.log('state.getCurrentPlayer(): ', state.getCurrentPlayer());
   render();
+  checkWinner();
+  // console.log(newBoard);
+  // console.log(newBoard);
   // checkMoves();
+  // console.log(checkMoves);
+  // winningMove();
+  // console.log(flattenList(state.board));
 
 })
 
